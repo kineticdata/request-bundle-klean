e75466869a53db5fb4f07ad0df9fd7b8f58dcb38
Shayne Koestler <shayne.koestler@kineticdata.com>
2011-12-23 10:43:23 -0600

    login page auto focuses username field on load (see enhancement #1928)

---
+++ packages/base/resources/js/login.js
@@ -0,0 +1,7 @@
+/*
+ * When the page loads, set the focus to the username input field.  Note that
+ * the id of the input element to set focus is 'UserName' in the login.jsp.
+ */
+window.onload = function() {
+    document.getElementById('UserName').focus();
+}
